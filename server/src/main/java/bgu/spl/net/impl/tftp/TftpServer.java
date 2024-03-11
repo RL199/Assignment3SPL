6fcc7d4d852fcbfed195813961220ddb59e04b6e
Roy Levi <roy3@post.bgu.ac.il>
2024-03-11 03:32:53 +0200

    Add connections and connectionIdCounter to TftpServer class

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpServer.java
@@ -6,16 +6,24 @@ import bgu.spl.net.api.MessagingProtocol;
 import bgu.spl.net.srv.BaseServer;
 import bgu.spl.net.srv.BlockingConnectionHandler;
 import java.util.function.Supplier;
+import bgu.spl.net.srv.Connections;
 
 public class TftpServer extends BaseServer<byte[]> {
 
+    private Connections<byte[]> connections;
+
+    int connectionIdCounter = 0;
+
     public TftpServer(int port, Supplier<BidiMessagingProtocol<byte[]>> protocolFactory, Supplier<MessageEncoderDecoder<byte[]>> encdecFactory) {
         super(port, protocolFactory, encdecFactory);
+        connections = new ConnectionsImpl<>();
     }
 
     @Override
     protected void execute(BlockingConnectionHandler<byte[]> handler) {
+        handler.start(connectionIdCounter++ ,connections);
         new Thread(handler).start();
+
     }
 
     public static void main(String[] args) {
@@ -23,7 +31,6 @@ public class TftpServer extends BaseServer<byte[]> {
                 7777,
                 TftpProtocol::new,
                 TftpEncoderDecoder::new
-
         );
         server.serve();
     }
